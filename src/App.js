8b93d643c5c24ccb51af41ec5f5eff3139fe56e6
jmge0511 <jmge0511@naver.com>
2022-04-20 05:53:41 +0900

    4장 완성 소스코드

--- src/App.js
+++ src/App.js
@@ -1,43 +1,62 @@
-function Food({name, picture}) {
+import PropTypes from "prop-types";
+
+function Food({name, picture, rating}) {
   return (
     <div>
       <h2>I like {name}</h2>
-      <img src={picture} />
+      <h4>{rating}/5.0</h4>
+      <img src={picture} alt={name} />
     </div>
   );
 }
 
 const foodILike = [
   {
+    id: 1,
     name: 'Kimchi',
     image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.4aBRBEMc36CPZmEg84X40wEsDI%26pid%3DApi&f=1',
+    rating: 5
   },
   {
+    id: 2,
     name: 'Samgyeopsal',
     image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.t0EoaEzeLC_kBW3Ha4u9EgHaF7%26pid%3DApi&f=1',
+    rating: 4.9
   },
   {
+    id: 3,
     name: 'Bibimbap',
     image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.msp47D4NIaFD4Lxy2rvlUAHaE7%26pid%3DApi&f=1',
+    rating: 4.8
   },
   {
+    id: 4,
     name: 'Doncasu',
     image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.mW17Z8uq6unL6xvle_X0YgHaHa%26pid%3DApi&f=1',
+    rating: 4.5
   },
   {
+    id: 5,
     name: 'Kimbap',
     image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.8e2egDI0m8ecpvi5uMHoxAHaHa%26pid%3DApi&f=1',
+    rating: 5
   }
 ];
 
 function App() {
   return (
     <div>
-      {foodILike.map(dish => (
-        <Food key={dish.name} name={dish.name} picture={dish.image} />
-      ))}
+      {foodILike.map(dish =>
+        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
+        )}
     </div>
   );
 }
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number
+};
 
 export default App;
